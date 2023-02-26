cb5b8d0cfee17f8bc7bab5bb729f9d9dfae28915
Yuriy Minashkin <y.minashkin@gmail.com>
2023-02-26 11:43:02 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
+import { data } from './data/users'
+import { User } from '../components/User/User';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <User user={data[0]}/>
   );
 };
---
+++ src/components/User/User.jsx
@@ -0,0 +1,3 @@
+export const User = ({user}) => {
+  console.log(user)
+}
